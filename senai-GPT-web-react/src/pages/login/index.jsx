51601bc27683fd7ee0e0af6da077ef3840f889ea
Joao Ricardo <joaoricardo1127@gmail.com>
2025-04-15 21:40:27 -0300

    ponto de save

--- senai-GPT-web-react/src/pages/login/index.jsx
+++ senai-GPT-web-react/src/pages/login/index.jsx
@@ -6,7 +6,31 @@ function Login() {
 
   return (
     <>
-      <h1>Login</h1>
+
+      <hearder></hearder>
+
+<main className="page-container">
+
+    <div className="robo-image">
+    </div>
+
+    <div className="Login-container">
+
+        <img className="logo" src="../assets/imgs/Chat.png" alt="Logo do SenaiGPT"/>
+
+        <h1 id="meutitulo" className="titulo">Login</h1>
+
+        <input className="inpt" type="email" placeholder="Insira o E-mail"/>
+        <input className="inpt" type="password" placeholder="Insira uma Senha"/>
+
+        <button className="btn">Entrar</button>
+
+    </div>
+
+
+
+</main>
+
     </>
   )
 }

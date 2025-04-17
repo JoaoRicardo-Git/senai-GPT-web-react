2e636900e8b01afaee038855cc6442abba674cbb
Joao Ricardo <joaoricardo1127@gmail.com>
2025-04-16 21:50:46 -0300

    Sistema de login

--- senai-GPT-web-react/src/pages/login/index.jsx
+++ senai-GPT-web-react/src/pages/login/index.jsx
@@ -1,35 +1,59 @@
 import "./login.css";
+import logo from "../../assets/imgs/Chat.png";
+import { useState } from "react";
 
 function Login() {
 
-  //JavaScript
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLoginClick = async () => {
+
+    let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", {
+
+    headers: {
+       "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+        email: email,
+        password: password
+    })
+
+  });
+  
+  console.log(response);
+
+}
 
   return (
     <>
 
-      <hearder></hearder>
+      <hearder>
 
-<main className="page-container">
+      </hearder>
 
-    <div className="robo-image">
-    </div>
+      <main className="page-container">
 
-    <div className="Login-container">
+        <div className="robo-image">
+        </div>
 
-        <img className="logo" src="../assets/imgs/Chat.png" alt="Logo do SenaiGPT"/>
+        <div className="Login-container">
 
-        <h1 id="meutitulo" className="titulo">Login</h1>
+          <img className="logo" src={logo} alt="Logo do SenaiGPT" />
 
-        <input className="inpt" type="email" placeholder="Insira o E-mail"/>
-        <input className="inpt" type="password" placeholder="Insira uma Senha"/>
+          <h1 id="meutitulo" className="titulo">Login</h1>
 
-        <button className="btn">Entrar</button>
+          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o E-mail" />
+          <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Insira uma Senha" />
 
-    </div>
+          <button className="btn" onClick={() => onLoginClick()}>Entrar</button>
+
+        </div>
 
 
 
-</main>
+      </main>
 
     </>
   )
